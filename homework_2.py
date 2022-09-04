2f8d357c84f56aa791359b0368158f1cabbeb82d
Logunova Margarita <logunovar@gmail.com>
2022-09-04 16:54:08 +0300

    заметила ошибку

--- homework_2.py
+++ homework_2.py
@@ -16,5 +16,5 @@ def test_one(open_browser):
 
 def test_second(open_browser):
     browser.open('https://google.com/ncr')
-    browser.element('[name="q"]').should(be.blank).type('Margo').press_enter()
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should_not(have.text('Selene - User-oriented Web UI browser tests in Python'))
