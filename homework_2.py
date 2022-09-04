315f503fbbb97ca05eb62ae7decf7c3830921733
Logunova Margarita <logunovar@gmail.com>
2022-09-04 15:30:26 +0300

    Homework lesson two:fixture

---
+++ homework_2.py
@@ -0,0 +1,20 @@
+from selene.support.shared import browser
+from selene import be, have
+import pytest
+
+
+@pytest.fixture()
+def open_browser():
+    browser.config.window_width = 1000
+    browser.config.window_height = 1200
+
+
+def test_one(open_browser):
+    browser.open('https://google.com/ncr')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_second(open_browser):
+    browser.open('https://google.com/ncr')
+    browser.element('[name="q"]').should(be.blank).type('Margo').press_enter()
+    browser.element('[id="search"]').should_not(have.text('Selene - User-oriented Web UI browser tests in Python'))
